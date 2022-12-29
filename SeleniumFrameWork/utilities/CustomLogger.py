64968cf4c0fc64d1d7ae14f1aa4fb95777e93036
mstrzelczyk4 <58236066+mstrzelczyk4@users.noreply.github.com>
2022-12-29 04:42:20 +0100

    Add files via upload

---
+++ SeleniumFrameWork/utilities/CustomLogger.py
@@ -0,0 +1,39 @@
+import inspect
+import logging
+import allure
+
+
+def custom_logger():
+    # 1.) This is used to get the  class / method name from where this customLogger method is called
+    log_name = inspect.stack()[1][3]
+
+    # 2.) Create the logging object and pass the log_name in it
+    logger = logging.getLogger(log_name)
+
+    # 3.) Set the Log level
+    logger.setLevel(logging.DEBUG)
+
+    # 4.) Create the file_handler to save the logs in the file
+    file_handler = logging.FileHandler("../reports/Code2lead.log", mode='a')
+
+    # 5.) Set the logLevel for file_handler
+    file_handler.setLevel(logging.DEBUG)
+
+    # 6.) Create the formatter in which format do you like to save the logs
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s',
+                                  datefmt='%d/%m/%y %I:%M:%S %p %A')
+
+    # 7.) Set the formatter to file_handler
+    file_handler.setFormatter(formatter)
+
+    # 8.) Add file handler to logging
+    logger.addHandler(file_handler)
+
+    #  9.) Finally return the logging object
+
+    return logger
+
+
+def allure_logs(text):
+    with allure.step(text):
+        pass
